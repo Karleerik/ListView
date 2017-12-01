2836a04792a2c3b6293419735115bb7a30370ebd
Karl-Eerik Pikk <karl-eerik.pikk@vikk.ee>
2017-12-01 12:10:20 +0200

    Failid

---
+++ Hamburger/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Hamburger
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+        private void MyListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Nupp1.IsSelected)
+            {
+                MyFrame.Navigate(typeof(page));
+            }
+            else if (Nupp2.IsSelected)
+            {
+                MyFrame.Navigate(typeof(page2));
+            }
+            else if (Nupp3.IsSelected)
+            {
+                MyFrame.Navigate(typeof(page3));
+            }
+        }
+
+        private void BackKey_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
